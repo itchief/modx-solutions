b6264b02f3b3199b79b640f239f93427c0dc82ac
itchief <alex.malcev1980@gmail.com>
2021-09-10 22:48:35 +1000

    Update

--- 01-creating-registration-on-processors/03-captcha.php
+++ 01-creating-registration-on-processors/03-captcha.php
@@ -1,32 +1,48 @@
 <?php
-require_once "../../core/model/modx/modx.class.php";
-$modx = new modX();
-$modx->initialize('web');
 
-session_start();
+define('USE_SESSION', false);
 
-// присваиваем PHP переменной captchastring строку символов
-$string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz';
-// получаем первые 6 символов после их перемешивания с помощью функции str_shuffle
-$captcha = substr(str_shuffle($string), 0, 6);
-// инициализация переменной сессии с помощью сгенерированной подстроки captchastring,
-// содержащей 6 символов
-$_SESSION['captcha'] = $captcha;
-// генерируем CAPTCHA
-// создаем новое изображение из файла background.png
-$image = imagecreatefrompng(dirname(__FILE__).'/background.png');
-// устанавливаем цвет (R-200, G-240, B-240) изображению, хранящемуся в $image
-$colour = imagecolorallocate($image, 66, 182, 66);
-// присваиваем переменной font название шрифта
-$font = dirname(__FILE__).'/oswald.ttf';
-// устанавливаем случайное число между -10 и 10 градусов для поворота текста
-$rotate = rand(-10, 10);
-// рисуем текст на изображении шрифтом TrueType (1 параметр - изображение ($image),
-// 2 - размер шрифта (18), 3 - угол поворота текста ($rotate),
-// 4, 5 - начальные координаты x и y для текста (18,30), 6 - индекс цвета ($colour),
-// 7 - путь к файлу шрифта ($font), 8 - текст ($captchastring)
-imagettftext($image, 36, $rotate, 56, 64, $colour, $font, $captcha);
-// передавать изображение будем в формате png
-header('Content-type: image/png');
-// выводим изображение
+// 1. Генерируем код капчи
+// 1.1. Устанавливаем символы, из которых будет составляться код капчи
+$chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz';
+// 1.2. Количество символов в капче
+$length = 6;
+// 1.3. Генерируем код
+$code = substr(str_shuffle($chars), 0, $length);
+
+if (USE_SESSION) {
+  // 2a. Используем сессию
+  session_start();
+  $_SESSION['captcha'] =  crypt($code, '$1$itchief$7');
+  session_destroy();
+} else {
+  // 2a. Используем куки (время действия 600 секунд)
+  $value = crypt($code, '$1$itchief$7');
+  $expires = time() + 600;
+  setcookie('captcha', $value, $expires, '/', 'test.ru', false, true);
+}
+
+// 3. Генерируем изображение
+// 3.1. Создаем новое изображение из файла
+$image = imagecreatefrompng(__DIR__ . '/files/bg.png');
+// 3.2 Устанавливаем размер шрифта в пунктах
+$size = 36;
+// 3.3. Создаём цвет, который будет использоваться в изображении
+$color = imagecolorallocate($image, 66, 182, 66);
+// 3.4. Устанавливаем путь к шрифту
+$font = __DIR__ . '/files//oswald.ttf';
+// 3.5 Задаём угол в градусах
+$angle = rand(-10, 10);
+// 3.6. Устанавливаем координаты точки для первого символа текста
+$x = 56;
+$y = 64;
+// 3.7. Наносим текст на изображение
+imagefttext($image, $size, $angle, $x, $y, $color, $font, $code);
+// 3.8 Устанавливаем заголовки
+header('Cache-Control: no-store, must-revalidate');
+header('Expires: 0');
+header('Content-Type: image/png');
+// 3.9. Выводим изображение
 imagepng($image);
+// 3.10. Удаляем изображение
+imagedestroy($image);
--- 01-creating-registration-on-processors/06-create-user.php
+++ 01-creating-registration-on-processors/06-create-user.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once 'config.core.php';
+require_once '../../config.core.php';
 
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 $modx = new modX();
 $modx->initialize('web');
 $modx->getService('error', 'error.modError', '', '');
 
-if ($modx->getOption('anonymous_sessions') === '0') {
-  session_start();
-}
+$captcha = $_COOKIE['captcha'];
+unset($_COOKIE['captcha']);
+setcookie('captcha', '', time() - 3600, '/', 'test.ru', false, true);
 
 $groups = [
   'Users' => [
@@ -27,7 +27,7 @@ $params = [
   'passwordnotifymethod' => 'e', // g - генерировать, else e
   'fullname' => $_POST['fullname'],
   'captcha' => $_POST['captcha'],
-  'correctcaptcha' => $_SESSION['captcha'],
+  'correctcaptcha' => $captcha,
   'extended' => ['registered' => date('Y-m-d H:i:s', time()), 'activation_key' => md5(uniqid(rand(), true))],
   'active' => false,
   'groups' => $groups
@@ -40,9 +40,8 @@ $response = $modx->runProcessor('ext/user/create', $params);
 
 if ($response->isError()) {
   $result['success'] = false;
-  $result['message'] = $response->getMessage();
   $result['errors'] = $response->response['errors'];
   $modx->log(modX::LOG_LEVEL_ERROR, 'User create error. ' . $response->getMessage());
 }
 
-exit(json_encode($result));
+echo json_encode($result);
